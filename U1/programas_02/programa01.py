dfe05422c90ba61409ed3af1934baed8936c282c
Miguel Durán <migueld202035@gmail.com>
2025-09-24 12:36:43 +0200

    U1 programas_02 | programa01 terminado, programa02 empezado

---
+++ U1/programas_02/programa01.py
@@ -0,0 +1,9 @@
+entero = 67
+decimal = 12.34
+boleano = False
+cadena = "\nif (Python == Love): print('Hola!')\nHola!"
+
+print(entero,type(entero))
+print(decimal,type(decimal))
+print(boleano,type(boleano))
+print(cadena,type(cadena))
