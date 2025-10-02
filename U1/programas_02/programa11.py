7a3085d21c8bdc19176621bf9e565e51d78329f2
Miguel Duran <migueld202035@gmail.com>
2025-10-02 16:34:42 +0200

    U1 programas_02 | terminado

--- U1/programas_02/programa11.py
+++ U1/programas_02/programa11.py
@@ -1,3 +1,23 @@
 '''Un ciclista parte de una ciudad A a las HH horas, MM minutos y SS segundos. El tiempo
 de viaje hasta llegar a otra ciudad B es de N segundos. Escribie un programa que
 determine la hora de llegada a la ciudad B.'''
+
+import math
+
+hora = int(input('Introduce la hora de salida ')) * 3600
+minuto = int(input('Introduce los minutos de salida ')) * 60
+segundos = int(input('Introduce los segundos de salida '))
+segundos_que_tarda = int(input('Introduce los segundos que tarda en llegar '))
+total = hora + minuto + segundos + segundos_que_tarda
+
+hora = math.floor(total / 3600)
+total = total % 3600
+
+minuto = math.floor(total / 60)
+total = total % 60
+
+segundos = total
+
+print('hora de llegada:',hora,minuto,segundos)
+
+
--- U1/programas_02/programa14.py
+++ U1/programas_02/programa14.py
@@ -1,2 +1,20 @@
-'''Escribe un programa que reciba un número de bytes y muestre por pantalla cuantos
-GBytes, MBytes, KBytes y Bytes son. Tanto para el sistema decimal como el binario'''
+bytes_iniciales = int(input('Introduce los bytes: '))
+
+resto = bytes_iniciales
+gb_dec = resto // 1000000000
+resto %= 1000000000
+mb_dec = resto // 1000000
+resto %= 1000000
+kb_dec = resto // 1000
+b_dec = resto % 1000
+
+resto = bytes_iniciales
+gb_bin = resto // (1024 ** 3)
+resto %= 1024 ** 3
+mb_bin = resto // (1024 ** 2)
+resto %= 1024 ** 2
+kb_bin = resto // 1024
+b_bin = resto % 1024
+
+print(f"{bytes_iniciales} bytes en sistema decimal (SI): {gb_dec} GB, {mb_dec} MB, {kb_dec} KB, {b_dec} bytes")
+print(f"{bytes_iniciales} bytes en sistema binario (IEC): {gb_bin} GiB, {mb_bin} MiB, {kb_bin} KiB, {b_bin} bytes")
