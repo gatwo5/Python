8a58dd9b42112b6e3648ab93dee0a5c5bf75955c
Miguel Durán <migueld202035@gmail.com>
2025-10-23 13:55:58 +0200

    U4 01_cadenas | programa01.py Terminado

---
+++ U4/01_cadenas/programa01.py
@@ -0,0 +1,11 @@
+cad1 = 'Hola!'
+cad2 = "Buenas!"
+cad3 = '''Encantado!'''
+
+print(cad1)
+print(cad2)
+print(cad3)
+
+print('Primer y último caracter de cad1:',cad1[0],cad1[-1])
+print('Primer y último caracter de cad2:',cad2[0],cad2[-1])
+print('Primer y último caracter de cad3:',cad3[0],cad3[-1])
