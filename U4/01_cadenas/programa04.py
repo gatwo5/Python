41e63dec593a7c60a3118c83fdaf39562cd18a01
Miguel Durán <migueld202035@gmail.com>
2025-11-19 12:43:09 +0100

    U4 01_cadenas | Hasta el 5 (sin terminar)

---
+++ U4/01_cadenas/programa04.py
@@ -0,0 +1,25 @@
+entero = 56
+decimal = 5.6
+booleano = False
+
+#pasar a str
+
+entero = str(entero)
+decimal = str(decimal)
+booleano = str(booleano)
+
+print(type(entero), type(decimal), type(booleano))
+
+#pasar a int, float y bool
+
+entero = int(entero)
+decimal = float(decimal)
+booleano = bool(booleano)
+
+print(type(entero), type(decimal), type(booleano))
+
+#cadena cruda
+
+cruda = r"Hola\nHola\nHola"
+
+print(cruda)
---
+++ U4/01_cadenas/programa05.py
@@ -0,0 +1,17 @@
+#concatenacion
+
+cad1 = 'hola'
+cad2 = 'buenos días'
+resultado = cad1 + cad2
+
+print(resultado)
+
+#repeticion
+
+print(resultado * 3)
+
+if (cad1 > cad2):
+    print(f"{cad1} es mayor que {cad2}")
+
+else:
+    print(f"{cad2} es mayor que {cad1}")
