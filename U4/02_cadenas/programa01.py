1c8c6d8260cd4fe156c2433c87901f99a2a3173a
Miguel Duran <migueld202035@gmail.com>
2025-12-09 21:48:28 +0100

    U4 02_cadenas | 2 y 3

--- U4/02_cadenas/programa01.py
+++ U4/02_cadenas/programa01.py
@@ -1,1 +1,8 @@
 credenciales = "usuario:root|contraseña:123456"
+
+usuario, contrasenia = credenciales.split('|')
+
+usuario = usuario.split(':')[1]
+contrasenia = contrasenia.split(":")[1]
+
+print(usuario, contrasenia)
---
+++ U4/02_cadenas/programa02.py
@@ -0,0 +1,5 @@
+clave = input("Introduzca su clave")
+caracter = input("Introduce el caracter")
+
+print(caracter*len(clave))
+
